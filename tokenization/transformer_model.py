0814ad45a3388222dada8034ddd48fe6e8922015
Daniel Ruijs <daniel.ruijs@gmail.com>
2025-05-06 14:41:39 +0000

    Add: word2vec
    Co-authored-by: Mattias Kvist <mattiaskvist@users.noreply.github.com>

---
+++ tokenization/transformer_model.py
@@ -0,0 +1,178 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+def positional_encoding(length, depth):
+    depth = depth / 2
+
+    positions = np.arange(length)[:, np.newaxis]  # (seq, 1)
+    depths = np.arange(depth)[np.newaxis, :] / depth  # (1, depth)
+
+    angle_rates = 1 / (10000**depths)  # (1, depth)
+    angle_rads = positions * angle_rates  # (pos, depth)
+
+    pos_encoding = np.concatenate([np.sin(angle_rads), np.cos(angle_rads)], axis=-1)
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+class PositionalEmbedding(keras.layers.Layer):
+    # d_model is the dimension of the embedding
+    def __init__(self, word_embeddings):
+        super().__init__()
+        vocab_size, self.d_model = word_embeddings.shape
+        self.embedding = keras.layers.Embedding(
+            input_dim=vocab_size,
+            output_dim=self.d_model,
+            weights=[word_embeddings],
+            trainable=False,
+            mask_zero=True,
+            name="pretrained_embedding",
+        )
+        self.pos_encoding = positional_encoding(length=2048, depth=self.d_model)
+
+    def call(self, x):
+        length = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.d_model, x.dtype))
+        x = x + tf.cast(self.pos_encoding[tf.newaxis, :length, :], x.dtype)
+        return x
+
+
+class BaseAttention(keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.layernorm = tf.keras.layers.LayerNormalization()
+        self.add = tf.keras.layers.Add()
+
+
+# First block in decoder
+class CausalSelfAttention(BaseAttention):
+    def call(self, x):
+        attn_output = self.mha(query=x, value=x, key=x, use_causal_mask=True)
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+        return x
+
+
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, d_model, dff, dropout_rate=0):
+        super().__init__()
+        self.seq = tf.keras.Sequential(
+            [
+                tf.keras.layers.Dense(dff, activation="relu"),
+                tf.keras.layers.Dense(d_model),
+                tf.keras.layers.Dropout(dropout_rate),
+            ]
+        )
+        self.add = tf.keras.layers.Add()
+        self.layer_norm = tf.keras.layers.LayerNormalization()
+
+    def call(self, x):
+        x = self.add([x, self.seq(x)])
+        x = self.layer_norm(x)
+        return x
+
+
+class DecoderLayer(tf.keras.layers.Layer):
+    def __init__(self, *, d_model, num_heads, dff, dropout_rate=0):
+        super().__init__()
+
+        self.causal_self_attention = CausalSelfAttention(
+            num_heads=num_heads, key_dim=d_model, dropout=dropout_rate
+        )
+
+        self.ffn = FeedForward(d_model, dff)
+
+    def call(self, x):
+        x = self.causal_self_attention(x=x)
+        x = self.ffn(x=x)
+        return x
+
+
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self, *, word_embeddings, num_layers, num_heads, dff, dropout_rate=0):
+        super().__init__()
+
+        self.d_model = word_embeddings.shape[1]
+        self.num_layers = num_layers
+
+        self.pos_embedding = PositionalEmbedding(word_embeddings)
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.dec_layers = [
+            DecoderLayer(
+                d_model=self.d_model,
+                num_heads=num_heads,
+                dff=dff,
+                dropout_rate=dropout_rate,
+            )
+            for _ in range(num_layers)
+        ]
+
+    def call(self, x):
+        x = self.pos_embedding(x)  # (batch_size, target_seq_len, d_model)
+        x = self.dropout(x)
+        for i in range(self.num_layers):
+            x = self.dec_layers[i](x)
+        # The shape of x is (batch_size, target_seq_len, d_model).
+        return x
+
+
+class Transformer(keras.Model):
+    def __init__(
+        self,
+        word_embeddings,
+        num_layers=1,
+        dff=2048,
+        num_heads=8,
+        dropout_rate=0,
+    ):
+        super().__init__()
+        self.decoder = Decoder(
+            word_embeddings=word_embeddings,
+            num_layers=num_layers,
+            num_heads=num_heads,
+            dff=dff,
+            dropout_rate=dropout_rate,
+        )
+        self.dense = keras.layers.Dense(word_embeddings.shape[0])
+
+    def call(self, x):
+        x = self.decoder(x)  # (batch_size, target_len, d_model)
+
+        logits = self.dense(x)  # (batch_size, target_len, target_vocab_size)
+
+        try:
+            # Drop the keras mask, so it doesn't scale the losses/metrics.
+            del logits._keras_mask
+        except AttributeError:
+            pass
+
+        return logits
+
+
+class TransformerOneStep(keras.Model):
+    def __init__(self, model, temperature=1.0):
+        super().__init__()
+        self.temperature = temperature
+        self.model = model
+
+    @tf.function
+    def generate_one_step(self, input_ids):
+        # Run the model.
+        # predicted_logits.shape is [batch, char, next_char_logits]
+        predicted_logits = self.model(x=input_ids)
+        # Only use the last prediction.
+        predicted_logits = predicted_logits[:, -1, :]
+        predicted_logits = predicted_logits / self.temperature
+
+        # Sample the output logits to generate token IDs.
+        predicted_ids = tf.random.categorical(
+            predicted_logits, num_samples=1, dtype=tf.int32
+        )
+        predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+
+        # Return the characters
+        return predicted_ids
